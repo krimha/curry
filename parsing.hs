ce98755516fc8ee7c6af0a1f0b39e049012d5d4a
Kristian Monsen Haug <kristian.haug95@gmail.com>
2018-06-01 09:54:43 +0200

    Implement basic parsing

---
+++ parsing.hs
@@ -0,0 +1,32 @@
+import Text.ParserCombinators.Parsec
+
+data Task = Task {
+  text :: String
+                 }
+
+instance Show Task where
+  show task = text task
+
+tasks :: Parser [Task]
+tasks =
+  do result <- many task
+     eof
+     return result
+
+task :: Parser Task
+task =
+  do spaces
+     string "TASK"
+     spaces
+     result <- line
+     eol
+     return Task { text=result }
+
+line :: Parser String
+line = many (noneOf "\n")
+
+eol :: Parser Char
+eol = char '\n'
+
+
+
